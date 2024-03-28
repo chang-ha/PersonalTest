15750d7b7466c842c1b79004a1781e7e42904af0
chang-ha <kym637@naver.com>
2024-03-28 13:11:06 +0900

    우창하 : Weak_Ptr

---
+++ Weak_Ptr/Weak_Ptr.cpp
@@ -0,0 +1,48 @@
+﻿#include<memory>
+#include <iostream>
+
+class Parent : public std::enable_shared_from_this<Parent>
+{
+public:
+	Parent()
+	{
+		std::cout << "Parent" << std::endl;
+	}
+
+	~Parent()
+	{
+		std::cout << "~Parent" << std::endl;
+	}
+
+	std::shared_ptr<class Child> mChild;
+};
+
+class Child : public std::enable_shared_from_this<Child>
+{
+public:
+	Child()
+	{
+		std::cout << "Child" << std::endl;
+	}
+
+	~Child()
+	{
+		std::cout << "~Child" << std::endl;
+	}
+
+	std::weak_ptr<Parent> mParent;
+};
+
+
+int main()
+{
+	std::shared_ptr<Parent> p = std::make_shared<Parent>();
+	std::shared_ptr<Child> c = std::make_shared<Child>();
+
+	p->mChild = c;
+	c->mParent = p;
+
+	long long address = reinterpret_cast<long long>(c->mParent);
+
+	int a = 0;
+}
